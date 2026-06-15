d5ff23a94c55abe3c3e3f888c110765691c75cd2
Nick Buckland <nicholas.buckland@equalexperts.com>
2026-06-15 16:00:06 +0100

    AMCR-244 - Update start-server to include frontend access link and certificates URL

--- src/server/common/helpers/start-server.js
+++ src/server/common/helpers/start-server.js
@@ -4,8 +4,18 @@ async function startServer() {
   const server = await createServer()
   await server.start()
 
+  const port = config.get('port')
+  const origin = `${server.info.protocol}://localhost:${port}`
+
   server.logger.info('Server started successfully')
+<<<<<<< HEAD
   server.logger.info(`Access your frontend on ${server.info.uri}`)
+=======
+  server.logger.info(`Access your frontend on ${origin}`)
+  server.logger.info(
+    `Certificates of compliance: ${origin}/certificates-of-compliance`
+  )
+>>>>>>> ec54979 (AMCR-244 - Update start-server to include frontend access link and certificates URL)
 
   return server
 }
