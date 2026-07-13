1388eca35c9a6de5f26a80f87aebea17d664c777
Daniel Ashton <daniel.ashton@dashkat.uk>
2026-07-13 23:23:34 +0100

    AMCR-309 - format fix

--- src/server/plugins/router.test.js
+++ src/server/plugins/router.test.js
@@ -1,4 +1,4 @@
-import { describe, it, expect, vi, beforeEach, afterEach } from 'vitest'
+import { describe, it, expect, vi, afterEach } from 'vitest'
 import inert from '@hapi/inert'
 
 import { config } from '#config/config.js'
