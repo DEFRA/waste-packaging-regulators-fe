94bb1d1b99489fcb61cea1caced1ebfb8fc069a5
Nick Buckland <nicholas.buckland@equalexperts.com>
2026-09-01 14:33:55 +0100

    Remove unused pageI18n wrapper from Nunjucks globals

--- src/config/nunjucks/globals/globals.js
+++ src/config/nunjucks/globals/globals.js
@@ -1,7 +1,4 @@
-import {
-  pageI18n as createPageI18n,
-  translate
-} from '#server/common/helpers/i18n/translate.js'
+import { translate } from '#server/common/helpers/i18n/translate.js'
 
 const govukRebrand = true
 
@@ -9,8 +6,4 @@ function t(locale, key, params = {}) {
   return translate(locale, key, params)
 }
 
-function pageI18n(locale, pageLocaleBase) {
-  return createPageI18n(locale, pageLocaleBase)
-}
-
-export { govukRebrand, pageI18n, t }
+export { govukRebrand, t }
